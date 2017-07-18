2a35d91d512051258f0ed091f702d717655d1f93
sandeepvarikunta <sandeepv3191@gmail.com>
2017-07-18 11:15:51 +0530

    Update HelloWorld.java

--- New Project-20170718/HelloWorld.java
+++ New Project-20170718/HelloWorld.java
@@ -1,5 +1,15 @@
 class HelloWorld{
     static int x = 5;
     int y = 15;
-    public static void main
+    public static void main (String []args)
+    {
+        HelloWorld Obj = new HelloWorld();
+        System.out.println(x);
+        System.out.println(HelloWorld.x);
+        System.out.println(Obj.x);
+        System.out.println(new HelloWorld().y);
+        System.out.println(Obj.y);
+        //System.out.println(HelloWorld.y);
+        
+    }
 }
